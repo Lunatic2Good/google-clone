8c9de6af54a5543be0582f90539a761ee653a5f4
Lunatic <himeshtyagi315@gmail.com>
2024-07-25 17:07:41 +0530

    add header, body

---
+++ src/app/components/HomeSearch.jsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+
+export default function HomeSearch() {
+    const [input, setInput] = useState("");
+    const router = useRouter();
+    const [randomSearchLoading, setRandomSearchLoading] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if(!input.trim()) return;
+        router.push(`/search/web?searchTerm=${input}`);
+    }
+
+    async function randomSearch(e) {
+        setRandomSearchLoading(true);
+        const response = await fetch("https://random-word-api.vercel.app/api?words=1");
+        if(!response) return;
+        const data = await response.json();
+        router.push(`/search/web?searchTerm=${data}`);
+        setRandomSearchLoading(false);
+    }
+
+  return (
+    <>
+        <form onSubmit={handleSubmit} className="flex items-center w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
+            <AiOutlineSearch className="text-xl text-gray-500 mr-3"/>
+            <input onChange={(e) => setInput(e.target.value)} type="text" value={input} className="flex-grow focus:outline-none" />
+            <Image width={32} height={44} src="/mic.svg"/>
+            <Image width={32} height={44} src="/lens.svg" className="ml-1"/>
+        </form>
+
+        <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8">
+            <button onClick={handleSubmit} className="btn">Google Search</button>
+            <button disabled={randomSearchLoading} onClick={randomSearch} className="btn flex items-center justify-center disabled:opacity-80">
+                {randomSearchLoading ? (
+                    <img src="/spinner.svg" alt="loading..." className="h-6 text-center"/>
+                ) : (
+                    "I'm Feeling Lucky"
+                )}
+            </button>
+        </div>
+    </>
+  )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+import HomeHeader from "./components/HomeHeader";
+import HomeSearch from "./components/HomeSearch";
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <HomeHeader/>
+
+      {/* body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image width={300} height={100} src="https://www.google.co.in/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png"/>
+        <HomeSearch/>
+      </div>
+    </>
   )
 }
